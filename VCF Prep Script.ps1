6ec65860e9d3d4635a8de643b71ac39abcf41ba0
AJ Kuftic <ajkuftic@gmail.com>
2019-02-15 09:49:37 -0500

    Updated script
    Added DNS configuration and shell warning suppression, removed the Remove All NTP step.

--- VCF Prep Script.ps1
+++ VCF Prep Script.ps1
@@ -7,13 +7,18 @@
 # the hosts are connected to the    #
 # network and are accessible.       #
 #                                   #
+#   ***Change the variables below   #
+#     your specific settings!***    #
 #####################################
-
 
 $pass = "VMware1!"
 $hostfile = "C:\VCFHosts.txt"
 $ntpone = "time1.domain.com"
 $ntptwo = "time2.domain.com"
+$dnsone = "8.8.8.8"
+$dnstwo = "8.8.4.4"
+$domainname = "domain.com"
+
 
 #DO NOT CHANGE BELOW THIS LINE#
 $hosts = Get-Content -Path $hostfile
@@ -21,16 +26,18 @@ ForEach ($esx in $hosts) {
     Write-Host "Connecting to $esx" -ForegroundColor Green 
     Connect-VIServer -Server $esx -User root -Password $pass
 
+    Write-Host "Configuring DNS and Domain Name on $esx" -ForegroundColor Green
+    Get-VMHostNetwork -VMHost $esx | Set-VMHostNetwork -DomainName $domainname -DNSAddress $dnsone , $dnstwo -Confirm:$false
+
     Write-Host "Enabling SSH on $esx" -ForegroundColor Green 
     Get-VMHostService -VMHost $esx | where{$_.Key -eq "TSM-SSH"} | Start-VMHostService -Confirm:$false
 
+    Write-Host "Suppressing Shell Warning on $esx" -ForegroundColor Green
+    Get-VMHost | Get-AdvancedSetting UserVars.SuppressShellWarning | Set-AdvancedSetting -Value 1 -Confirm:$false
+
     Write-Host "Configuring SSH Client Policy on $esx" -ForegroundColor Green 
     Get-VMHostService -VMHost $esx | where {$_.Key -eq "TSM-SSH"} | Set-VMHostService -policy "on" -Confirm:$false 
-
-    Write-Host "Remove NTP Servers on $esx" -ForegroundColor Green 
-    $allNTPList = Get-VMHostNtpServer -VMHost $esx
-    Remove-VMHostNtpServer -VMHost $esx -NtpServer $allNTPList -Confirm:$false | out-null
-        
+      
     Write-Host "Adding NTP Server $ntpone on $esx" -ForegroundColor Green 
     Add-VMHostNTPServer -NtpServer $ntpone -VMHost $esx -Confirm:$false
 
